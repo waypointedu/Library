ac7fef2cf63f4bb8a05b54aa8dda76df6e7bd85e
Waypoint Institute <institutewaypoint@gmail.com>
2026-03-12 20:40:36 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
+import FacultyProfile from './pages/FacultyProfile';
+import FacultyProfileEdit from './pages/FacultyProfileEdit';
 
 const { Pages, Layout, mainPage } = pagesConfig;
 const mainPageKey = mainPage ?? Object.keys(Pages)[0];
